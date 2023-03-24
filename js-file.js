bea76254e8b536cd9e3d49151e769578359a88ab
sitchdallas <125512025+sitchdallas@users.noreply.github.com>
2023-03-23 19:13:54 -0500

    added an event listener which creates a drawing effect

--- js-file.js
+++ js-file.js
@@ -6,10 +6,12 @@ function makeRows(gridNumber) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        cell.innerText = ('');
-        container.appendChild(cell).className = "gridSquare";
-    };
+        let gridSquare = document.createElement("div");
+        container.appendChild(gridSquare).className = "gridSquare";
+        gridSquare.addEventListener('mouseover', function(e) {
+            this.style.backgroundColor = 'black'
+        }
+    )};
 };
 
-makeRows(16);
+makeRows(64);
